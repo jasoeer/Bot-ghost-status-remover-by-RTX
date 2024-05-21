aa903e10d4d374550e70f8a7e7845e5979996b12
jasper11223131 <163884717+jasper11223131@users.noreply.github.com>
2024-05-21 10:30:48 +0600

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching to Demon's Heaven | /kick "];
+const statusMessages = ["Watching to Demon's Heaven"];
 
 
 let currentIndex = 0;
