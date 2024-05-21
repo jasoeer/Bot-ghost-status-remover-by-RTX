dc155c6338287a090ab3f262df28eeac726402c2
jasper11223131 <163884717+jasper11223131@users.noreply.github.com>
2024-05-21 10:23:28 +0600

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching to ServerSpark | /kick "];
+const statusMessages = ["Watching to Demon's Heaven | /kick "];
 
 
 let currentIndex = 0;
